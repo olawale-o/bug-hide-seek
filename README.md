34952b958cd20c0534ab5e4c2b76773dfc552aa7
olawale-o <omoogunolawale@gmail.com>
2021-06-25 13:27:52 +0100

    Add bug.js index.html and README.md Files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bugs: Hide and seek
+
+> This project is about detecting and fixing bugs
---
+++ bug.js
@@ -0,0 +1,25 @@
+
+const studentsAge = [23,21,32,26];
+
+const studentDetails = {
+  name: "olawale",
+  age: "23",
+};
+
+console.log(studentsAge['hobbies']);
+
+for (let i = 0; i <= studentsAge.length; i++) {
+    newArr.push(studentsAge[i])
+}
+
+function () {
+
+    return x + y
+}
+
+const btn = document.querySelector('#btn')
+
+btn.addEventListener('scroll', () => {
+    
+});
+
